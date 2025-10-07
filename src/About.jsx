706be071913f9e82b3f7d4ad66fe341439be1101
MaryjeC <clasinmaryje56@gmail.com>
2025-10-07 19:27:34 +0800

    edited About

--- src/About.jsx
+++ src/About.jsx
@@ -1,6 +1,13 @@
 
 import "./index.css";
 import pnc from "./images/pnc.png";
+import je from "./images/je.jpg";
+import MIR from "./images/MIR.jpg";
+import rose from "./images/rose.jpg";
+import jaz from "./images/jaz.jpg";
+import mj from "./images/mj.jpg";
+import jii from "./images/jii.jpg";
+import jc from "./images/jc.jpg";
 
 function About() {
   return (
@@ -11,6 +18,7 @@ function About() {
       <div className="members-con">
         <div className="member-card">
           <h5>Miriam Mulawin</h5>
+          <img src={MIR} alt="" className="profile-pic"/>
           <p><em>
             "A dedicated IT student who believes that every line of code
             brings her closer to achieving her goals"</em>
@@ -22,6 +30,7 @@ function About() {
 
         <div className="member-card">
           <h5>Rose Bren Dungo</h5>
+          <img src={rose} alt="" className="profile-pic"/>
           <p><em>"A determined IT student preparing to be part of the digital future."</em>
           </p>
            <p>Position: Member<br></br>
@@ -30,6 +39,7 @@ function About() {
 
         <div className="member-card">
           <h5>Jazmine Cruz</h5>
+          <img src={jaz} alt="" className="profile-pic "/>
           <p><em>
             "An IT student who sees technology as a tool to turn small ideas into
             big possibilities"</em>
@@ -40,6 +50,7 @@ function About() {
 
         <div className="member-card">
           <h5>Maryje Clasin</h5>
+          <img src={je} alt="" className="profile-pic" />
           <p><em>
             "Information Technology turns imagination into innovation,
             building bridges between people and possibilities"</em>
@@ -50,6 +61,7 @@ function About() {
 
         <div className="member-card">
           <h5>Michael Velasquez</h5>
+          <img src= {mj} alt="" className="profile-pic" />
           <p><em>
             "As both an artist and IT student, I am passionate about transforming
             ideas into creative, functional solutions that bridge design and
@@ -61,6 +73,7 @@ function About() {
 
         <div className="member-card">
           <h5>Jehus Cabalejo</h5>
+          <img src={jii} alt="" className="profile-pic " />
           <p><em>
             "As an IT student, I'm defining my future with every line of code I
             write."</em>
@@ -71,6 +84,7 @@ function About() {
 
         <div className="member-card">
           <h5>Jesus Cenido</h5>
+          <img src={jc} alt="" className="profile-pic" />
           <p><em>
             "As an IT student, I'm defining my future with every line of code I
             write."</em>
